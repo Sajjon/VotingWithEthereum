26c7beb2ce08c3d1d0751205dc5aef609fc6e19b
Sajjon <alex.cyon@gmail.com>
2017-11-18 20:05:38 +0100

    Broken wip

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,18 +2,48 @@ pragma solidity ^0.4.18;
 
 contract Voting {
 
-	uint8 public voteCount = 0;
+	Party[] public parties;
+	mapping(bytes32 => Party) public partyByName;
 
-	function getVoteCount() 
+	struct Party {
+		bytes32 name;
+		uint voteCount;
+	}
+
+	function Voting(bytes32[] partyNames) public {
+		for (uint i = 0; i < partyNames.length; i++) {
+			parties.push(Party({
+				name: partyNames[i],
+				voteCount: 0
+			}));
+		}
+	}
+
+	function getVoteCountForParty(bytes32 partyName) 
 		public
 		view
-		returns (uint8) 
+		returns (uint)
 	{
-	  return voteCount;
+	  return partyByName[partyName].voteCount;
 	}
 
-	function vote() public {
-		voteCount += 1;
+	function voteForParty(bytes32 partyName) 
+		public
+	{
+		require(partyByName[partyName].name.length > 0);
+		partyByName[partyName].voteCount += 1;
 	}
+
+	function getName(bytes32 partyName) 
+		public
+		view
+		returns (bytes32)
+	{
+		return partyByName[partyName].name;
+	}
+
+	// function getWinner() public view return (Party) {
+
+	// }
 
 }
--- test/TestVoting.sol
+++ test/TestVoting.sol
@@ -7,11 +7,36 @@ import "../contracts/Voting.sol";
 contract TestVoting {
 	Voting voting = Voting(DeployedAddresses.Voting());
 
-	function testVoting() public {
-		uint8 expected = 0;
-		uint8 voteCount = voting.voteCount;
-		Assert.equal(voteCount, expected, "Vote count should start at 0");
-		voting.vote();
-		// Assert.equal(voting.voteCount, 1, "Vote count should be 1 after voting");
+	bytes32 constant ethereumPartyName = "Ethereum Party";
+	bytes32 constant bitcoinPartyName = "Bitcoin Party";
+
+	function _testVotingForParty() public {
+		uint expected = 0;
+
+		Assert.equal(voting.getVoteCountForParty(ethereumPartyName), expected, "Vote count for party A should start at 0");
+		voting.voteForParty(ethereumPartyName);
+		expected = 1;
+		Assert.equal(voting.getVoteCountForParty(ethereumPartyName), expected, "Vote count for party A should be 1 after voting");
+		expected = 0;
+		Assert.equal(voting.getVoteCountForParty(bitcoinPartyName), expected, "Vote count for party B should be 0");
 	}
+
+	function testGetName() public {
+		bytes32 nonExistingParty = "nonExistingParty";
+		bytes32 expected = "";
+		bytes nameFromVoting = voting.getName(nonExistingParty);
+		Assert.equal(nameFromVoting, expected, "name should be empty");
+		uint expectedLength = 0;
+		Assert.equal(nameFromVoting.length, expectedLength, "name should have zero length");
+	}
+
+	// function testVotingForNonExistingParty() public {
+	// 	bytes32 nonExistingParty = "foobar";
+	// 	uint expected = 0;
+
+	// 	Assert.equal(voting.getVoteCountForParty(nonExistingParty), expected, "should be 0");
+	// 	voting.voteForParty(nonExistingParty);
+	// 	Assert.equal(voting.getVoteCountForParty(nonExistingParty), expected, "should still be 0");
+		
+	// }
 }
